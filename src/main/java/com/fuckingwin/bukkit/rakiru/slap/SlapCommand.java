25b60d287af7fb2b79830aef7ebf7324c39e83b1
Sean Gordon <sean_aldon@hotmail.co.uk>
2011-05-10 21:59:00 +0100

    Initial commit

---
+++ src/main/java/com/fuckingwin/bukkit/rakiru/slap/SlapCommand.java
@@ -0,0 +1,85 @@
+package com.fuckingwin.bukkit.rakiru.slap;
+
+import java.util.List;
+import java.util.Random;
+import org.bukkit.util.Vector;
+import org.bukkit.entity.Player;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.ChatColor;
+
+/**
+ * Handler for the /slap command.
+ * @author rakiru
+ */
+public class SlapCommand implements CommandExecutor {
+
+    private final SlapPlugin plugin;
+
+    public SlapCommand(SlapPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender slapper, Command command, String label, String[] split) {
+        if (plugin.usePermissions) {
+            if (!(plugin.permissionHandler.has((Player)slapper, "slap.slap"))) {
+                slapper.sendMessage(ChatColor.RED + "You do not have permission to use this command");
+                return true;
+            }
+        } else if (!(slapper.isOp())) {
+            slapper.sendMessage(ChatColor.RED + "You do not have permission to use this command");
+            return true;
+        }
+        String slapperName;
+        if (slapper instanceof Player) {
+            Player slapperCast = (Player)slapper;
+            slapperName = slapperCast.getDisplayName();
+        } else if (slapper instanceof ConsoleCommandSender) {
+            slapperName = "Console";
+        } else {
+            slapperName = "Someone";
+        }
+        if (split.length == 1 || split.length == 2) {
+            List<Player> matchedPlayers = plugin.getServer().matchPlayer(split[0]);
+            String slappeeName = split[0];
+            if (matchedPlayers.size() == 1) {
+                //player found with no force argument
+                if (split.length == 1) {
+                    slapPlayer(matchedPlayers.get(0),2,slapperName);
+                } else {
+                    float force = Float.parseFloat(split[1]);
+                    if (force > 10) {
+                        force = 10;
+                    } else if (force <= 0) {
+                        force = 0.1f;
+                    }
+                    slapPlayer(matchedPlayers.get(0),force,slapperName);
+                }
+            } else if (matchedPlayers.isEmpty()) {
+                //player not found
+                slapper.sendMessage("Player " + slappeeName + " not found");
+            } else if (matchedPlayers.size() > 1 ) {
+                //Multiple matching players found
+               slapper.sendMessage(slappeeName + " matches multiple players");
+            } else {
+                //Unknown error
+                slapper.sendMessage("Ah, crap!");
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void slapPlayer(Player slappee, float force, String slapperName) {
+        Random randomGen = new Random();
+        Vector newVelocity = new Vector((randomGen.nextFloat()*1.5-0.75)*force,randomGen.nextFloat()/2.5+(0.4*force),(randomGen.nextFloat()*1.5-0.75)*force);
+        slappee.setVelocity(newVelocity);
+        String slappeeName = slappee.getDisplayName();
+        //Have a switch/select here for "slapped X hard!" or "bitchslapped X!"
+        plugin.getServer().broadcastMessage(slapperName + " slapped " + slappeeName + "!");
+    }
+}
---
+++ src/main/java/com/fuckingwin/bukkit/rakiru/slap/SlapPlugin.java
@@ -0,0 +1,54 @@
+package com.fuckingwin.bukkit.rakiru.slap;
+
+import java.util.HashMap;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+import org.bukkit.plugin.Plugin;
+import java.util.logging.Logger;
+
+/**
+ * Slap plugin for bukkit
+ *
+ * @author rakiru
+ */
+public class SlapPlugin extends JavaPlugin {
+
+    public static PermissionHandler permissionHandler;
+    public boolean usePermissions;
+    protected final static Logger log = Logger.getLogger("Minecraft.Slap");
+
+    public void onDisable() {
+        // Output to console that plugin is disabled
+        PluginDescriptionFile pdfFile = this.getDescription();
+        System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " disabled!");
+    }
+
+    public void onEnable() {
+        // Get plugin info from plugin.yml
+        PluginDescriptionFile pdfFile = this.getDescription();
+
+        // Setup permissions
+        Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+
+        if (permissionsPlugin == null) {
+            log.info("[" + pdfFile.getName() + "] Permission system not detected, defaulting to OP");
+            usePermissions = false;
+        } else {
+            log.info("[" + pdfFile.getName() + "] Permission system detected");
+            usePermissions = true;
+            this.permissionHandler = ((Permissions) permissionsPlugin).getHandler();
+        }
+
+        // Register commands
+        getCommand("slap").setExecutor(new SlapCommand(this));
+
+        // Output to console that plugin is enabled
+        log.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " enabled!");
+    }
+}
